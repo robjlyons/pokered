22dd6c2356381344cf798a46164da54096b75593
robjlyons <robjlyons87@gmail.com>
2024-10-14 15:11:14 +0100

    ai improvements v19

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -1044,12 +1044,12 @@ FindBetterMatchup:
     
     ; Check if this Pokemon is fainted
     ld a, [hl]
-	ld a, [hl + c]
+    ld a, [hl+c]
     jr z, .nextMon  ; Skip if fainted
     
     ; Compare types
     ld a, [hl]
-	ld a, [hl + c]
+    ld a, [hl+c]
     call CalculateTypeEffectiveness
     cp 2
     jr nc, .betterMatchup  ; If super effective, we found a better matchup
